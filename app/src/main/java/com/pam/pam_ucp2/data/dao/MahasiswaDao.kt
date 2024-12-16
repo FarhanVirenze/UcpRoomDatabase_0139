df6348cff9f293b9d620db6dff76378c77911309
farhan <farhanvirenze18@gmail.com>
2024-12-16 17:00:51 +0700

    Membuat Interface MahasiswaDao

---
+++ app/src/main/java/com/pam/pam_ucp2/data/dao/MahasiswaDao.kt
@@ -0,0 +1,29 @@
+package com.pam.pam_ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.pam.pam_ucp2.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MahasiswaDao {
+    @Insert
+    suspend fun insertMahasiswa(
+        mahasiswa: Mahasiswa
+    )
+    //getAllMahasiswa
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMhs() : Flow<List<Mahasiswa>>
+    //getMahasiswa
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+    //deleteMahasiswa
+    @Delete
+    suspend fun deleteMahasiswa (mahasiswa: Mahasiswa)
+    //updateMahasiswa
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+}
