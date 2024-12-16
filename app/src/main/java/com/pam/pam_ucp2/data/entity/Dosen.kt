dadbe70e436c3a427a4fd188701e05c72892342e
farhan <farhanvirenze18@gmail.com>
2024-12-16 17:33:18 +0700

    membuat dataclass Dosen

---
+++ app/src/main/java/com/pam/pam_ucp2/data/entity/Dosen.kt
@@ -0,0 +1,12 @@
+package com.pam.pam_ucp2.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "dosen")
+data class Dosen(
+    @PrimaryKey
+    val nidn: String,
+    val nama: String,
+    val jeniskelamin: String
+)
