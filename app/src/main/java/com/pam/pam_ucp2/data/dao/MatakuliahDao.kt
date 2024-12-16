e5e062101e6d8c3efb3089db597ef843ef23ee2b
farhan <farhanvirenze18@gmail.com>
2024-12-16 17:34:22 +0700

    memmbuat MatakuliahDao

---
+++ app/src/main/java/com/pam/pam_ucp2/data/dao/MatakuliahDao.kt
@@ -0,0 +1,29 @@
+package com.pam.pam_ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.pam.pam_ucp2.data.entity.Matakuliah
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MatakuliahDao {
+    @Insert
+    suspend fun insertMatakuliah(
+        matakuliah: Matakuliah
+    )
+    //getAllMatakuliah
+    @Query("SELECT * FROM matakuliah ORDER BY nama ASC")
+    fun getAllMatakuliah() : Flow<List<Matakuliah>>
+    //getMatakuliah
+    @Query("SELECT * FROM matakuliah WHERE kode = :kode")
+    fun getMatakuliah(kode: String): Flow<Matakuliah>
+    //deleteMatakuliah
+    @Delete
+    suspend fun deleteMatakuliah (matakuliah: Matakuliah)
+    //updateMatakuliah
+    @Update
+    suspend fun updateMatakuliah (matakuliah: Matakuliah)
+}
