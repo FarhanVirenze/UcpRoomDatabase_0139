2239f712a4066d0932c7702c45b8ab354654bc07
farhan <farhanvirenze18@gmail.com>
2024-12-17 21:10:43 +0700

    Membuat LocalRepositoryMatakuliah

--- app/src/main/java/com/pam/pam_ucp2/repository/LocalRepositoryMatakuliah.kt
+++ app/src/main/java/com/pam/pam_ucp2/repository/LocalRepositoryMatakuliah.kt
@@ -1,4 +1,29 @@
 package com.pam.pam_ucp2.repository
 
-class LocalRepositoryMatakuliah {
+import com.pam.pam_ucp2.data.dao.MatakuliahDao
+import com.pam.pam_ucp2.data.entity.Matakuliah
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryMatakuliah(
+    private val matakuliahDao: MatakuliahDao
+) : RepositoryMatakuliah {
+    override suspend fun insertMatakuliah(matakuliah: Matakuliah) {
+        matakuliahDao.insertMatakuliah(matakuliah)
+    }
+
+    override fun getAllMatakuliah(): Flow<List<Matakuliah>> {
+        return matakuliahDao.getAllMatakuliah()
+    }
+
+    override fun getMhs(kode: String): Flow<Matakuliah> {
+        return matakuliahDao.getMatakuliah(kode)
+    }
+
+    override suspend fun deleteMatakuliah(matakuliah: Matakuliah) {
+        matakuliahDao.deleteMatakuliah(matakuliah)
+    }
+
+    override suspend fun updateMatakuliah(matakuliah: Matakuliah) {
+        matakuliahDao.updateMatakuliah(matakuliah)
+    }
 }
