8cfb82668a540875df97d0d870a8a7f2db9143ff
farhan <farhanvirenze18@gmail.com>
2024-12-17 21:10:54 +0700

    Membuat RepositoryMatakuliah

--- app/src/main/java/com/pam/pam_ucp2/repository/RepositoryMatakuliah.kt
+++ app/src/main/java/com/pam/pam_ucp2/repository/RepositoryMatakuliah.kt
@@ -1,4 +1,16 @@
 package com.pam.pam_ucp2.repository
 
+import com.pam.pam_ucp2.data.entity.Matakuliah
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryMatakuliah {
+    suspend fun insertMatakuliah(matakuliah: Matakuliah)
+    //getAllMatakuliah
+    fun getAllMatakuliah(): Flow<List<Matakuliah>>
+    //getMatakuliah
+    fun getMhs(kode: String): Flow<Matakuliah>
+    //deleteMatakuliah
+    suspend fun deleteMatakuliah(matakuliah: Matakuliah)
+    //updateMatakuliah
+    suspend fun updateMatakuliah(matakuliah: Matakuliah)
 }
