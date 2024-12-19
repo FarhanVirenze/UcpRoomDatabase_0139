7af5aa46bb90df496001a0b6424679b3b2d85279
farhan <farhanvirenze18@gmail.com>
2024-12-19 13:14:13 +0700

    Fixed RepositoryMatakuliah.kt

--- app/src/main/java/com/pam/pam_ucp2/repository/RepositoryMatakuliah.kt
+++ app/src/main/java/com/pam/pam_ucp2/repository/RepositoryMatakuliah.kt
@@ -4,13 +4,13 @@ import com.pam.pam_ucp2.data.entity.Matakuliah
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMatakuliah {
-    suspend fun insertMatakuliah(matakuliah: Matakuliah)
+    suspend fun insertMtk(matakuliah: Matakuliah)
     //getAllMatakuliah
-    fun getAllMatakuliah(): Flow<List<Matakuliah>>
+    fun getAllMtk(): Flow<List<Matakuliah>>
     //getMatakuliah
-    fun getMhs(kode: String): Flow<Matakuliah>
+    fun getMatakuliah(kode: String): Flow<Matakuliah>
     //deleteMatakuliah
-    suspend fun deleteMatakuliah(matakuliah: Matakuliah)
+    suspend fun deleteMtk(matakuliah: Matakuliah)
     //updateMatakuliah
-    suspend fun updateMatakuliah(matakuliah: Matakuliah)
+    suspend fun updateMtk(matakuliah: Matakuliah)
 }
