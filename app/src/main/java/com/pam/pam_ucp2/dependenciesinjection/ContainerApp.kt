17b5d40a8c2032142ae080c33b4b78a7a947394e
farhan <farhanvirenze18@gmail.com>
2024-12-19 13:04:18 +0700

    Membuat ContainerApp.kt

--- app/src/main/java/com/pam/pam_ucp2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/pam/pam_ucp2/dependenciesinjection/ContainerApp.kt
@@ -7,15 +7,21 @@ import com.pam.pam_ucp2.repository.LocalRepositoryMatakuliah
 import com.pam.pam_ucp2.repository.RepositoryDosen
 import com.pam.pam_ucp2.repository.RepositoryMatakuliah
 
+// Interface untuk Dependency Injection
 interface InterfaceContainerApp {
     val repositoryDosen: RepositoryDosen
     val repositoryMatakuliah: RepositoryMatakuliah
 }
 
+// Implementasi InterfaceContainerApp
 class ContainerApp(private val context: Context) : InterfaceContainerApp {
+
+    // Inisialisasi RepositoryDosen menggunakan DAO yang relevan
     override val repositoryDosen: RepositoryDosen by lazy {
         LocalRepositoryDosen(KrsDatabase.getDatabase(context).dosenDao())
     }
+
+    // Inisialisasi RepositoryMatakuliah menggunakan DAO yang relevan
     override val repositoryMatakuliah: RepositoryMatakuliah by lazy {
         LocalRepositoryMatakuliah(KrsDatabase.getDatabase(context).matakuliahDao())
     }
